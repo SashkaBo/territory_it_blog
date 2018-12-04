9d7d1c8e99bf87883e0b7d3ad5e719cf5d327c06
Aleksandra Bondarenko <al.s.bondarenko@gmail.com>
2018-12-04 17:51:30 +0300

    comment/remove useless

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,47 +2,39 @@ class PostsController < ApplicationController
   before_action :set_post, only: :rate
 
   def top_rated
-    @posts = Post.all
-    # if @posts.empty?
+    # @posts = Post.all
+    # # if @posts.empty?
 
-    render json: @posts
+    # render json: @posts
   end
 
   def create
     binding.pry
-    # puts 1
     @post = PostForm.new
-    # (permitted_params)
-
-    # if @post.save
-    #   render json: @post, status: :created, location: @post
-    # else
-    #   render json: @post.errors, status: :unprocessable_entity
-    # end
   end
 
   def rate
-    if @post.rate(permitted_params[:rating])
-      render json: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
+  #   if @post.rate(permitted_params[:rating])
+  #     render json: @post
+  #   else
+  #     render json: @post.errors, status: :unprocessable_entity
+  #   end
   end
 
   private
 
-  def permitted_params
-    params.permit(
-      :id,
-      :rating,
-      :title,
-      :content,
-      :user_ip,
-      :user_login
-    )
-  end
+  # def permitted_params
+  #   params.permit(
+  #     :id,
+  #     :rating,
+  #     :title,
+  #     :content,
+  #     :user_ip,
+  #     :user_login
+  #   )
+  # end
 
   def set_post
-    @post = Post.find(permitted_params[:id])
+  #   @post = Post.find(permitted_params[:id])
   end
 end
